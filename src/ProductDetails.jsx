9bb0862ebf85e826a14ee2567752d383d8d53d1f
swapnil042 <sheikhswapnil.musa@gmail.com>
2021-02-26 14:49:55 +0600

    Some Modifications

--- src/ProductDetails.jsx
+++ src/ProductDetails.jsx
@@ -11,16 +11,21 @@ const ProductDetails = (props)=>{
         },2000);
     },[])
 
+    let productDetails = <Spinner/>
+
+    if(loader === false){
+        productDetails = (<div style={{margin: 'auto', maxWidth: '40%', border: '2px solid grey', boxShadow: '5px 5px grey', borderRadius: '5px', padding: '20px' }}>
+                            <h3>Product Details</h3>
+                            <p><b>Product Name: </b>{props.details.name}</p>
+                            <p><b>Price: </b>{props.details.price}</p>
+                            <p><b>Details: </b>{props.details.description}</p>
+                            <p><b>Category: </b>{props.details.category}</p>
+                            <button className="button" onClick={props.onBack}>Go Back</button>
+                        </div>);
+    }
     return(
         <>
-            {loader === true ? <Spinner/> : (<div style={{margin: 'auto', maxWidth: '40%', border: '2px solid grey', boxShadow: '5px 5px grey', borderRadius: '5px', padding: '20px' }}>
-                                            <h3>Product Details</h3>
-                                            <p><b>Product Name: </b>{props.details.name}</p>
-                                            <p><b>Price: </b>{props.details.price}</p>
-                                            <p><b>Details: </b>{props.details.description}</p>
-                                            <p><b>Category: </b>{props.details.category}</p>
-                                            <button className="button" onClick={props.onBack}>Go Back</button>
-                                        </div>)}
+            {productDetails }
         </>
     )
 }
--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import ProductDetails from './ProductDetails';
-import Spinner from './Spinner';
 
 const ProductPage = (props) => {
     const [product, setProduct] = useState([
